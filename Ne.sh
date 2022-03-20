28dc37d764af1350c4da6c8c63f095085ec127c8
Teorooa <101207025+Teorooa@users.noreply.github.com>
2022-03-20 15:54:33 -0500

    Create Ne.sh

---
+++ Ne.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+zona
+printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m List finish \e[0m\n"
+elif [[ $option == 740 ]]; then
+numero
+read
+elif [[ $option == 741 ]]; then
+dgito
+read
+elif [[ $option == 742 ]]; then
+aplicacion
+read
+elif [[ $option == 743 ]]; then
+destino
+read
+elif [[ $option == 744 ]]; then
+seleccion
+read
+elif [[ $option == 745 ]]; then
+exelente
+read
+elif [[ $option == 746 ]]; then
+perfil
+read
+elif [[ $option == 747 ]]; then
+soporte
+read
+elif [[ $option == 748 ]]; then
+aporte
+read
+elif [[ $option == 749 ]]; then
+estable
+read
+elif [[ $option == 750 ]]; then
+principal
+read
+elif [[ $option == 751 ]]; then
+estar
+read
+elif [[ $option == 752 ]]; then
+factible
+read
+elif [[ $option == 753 ]]; then
+factor
+read
+
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+
+if [[ $option == 740 ]]; then
+numero
+read
+elif [[ $option == 741 ]]; then
+dgito
+read
+elif [[ $option == 742 ]]; then
+aplicacion
+read
+elif [[ $option == 743 ]]; then
+destino
+read
+elif [[ $option == 744 ]]; then
+seleccion
+read
+elif [[ $option == 745 ]]; then
+exelente
+read
+elif [[ $option == 746 ]]; then
+perfil
+read
+elif [[ $option == 747 ]]; then
+soporte
+read
+elif [[ $option == 748 ]]; then
+aporte
+read
+elif [[ $option == 749 ]]; then
+estable
+read
+elif [[ $option == 750 ]]; then
+principal
+read
+elif [[ $option == 751 ]]; then
+estar
+read
+elif [[ $option == 752 ]]; then
+factible
+read
+elif [[ $option == 753 ]]; then
+factor
+read
+elif [[ $option ]]; then
+read
+elif [[ $option ]]; then
+read
+elif [[ $option ]]; then
+read
+fi
+
+if [[ $option == 740 ]]; then
+numero
+read
+elif [[ $option == 741 ]]; then
+dgito
+read
+elif [[ $option == 742 ]]; then
+aplicacion
+read
+elif [[ $option == 743 ]]; then
+destino
+read
+
+if [[ $option == 744 ]]; then
+seleccion
+read
+elif [[ $option == 745 ]]; then
+exelente
+read
+elif [[ $option == 746 ]]; then
+perfil
+read
+elif [[ $option == 747 ]]; then
+soporte
+read
+fi
+
